812caba7f828c7adc8e4951a82e8586e2e0f5d19
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-02-13 15:51:31 +0000

    [FEAT] collection page

--- client/src/app/collection/page.tsx
+++ client/src/app/collection/page.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { useFetchOwnedCards } from "@/hooks/useFetchOwnedCards";
 import Image from "next/image";
 import { ethers } from "ethers";
-import { Dialog, DialogContent, DialogTitle, DialogClose } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
 import { PokemonMetadata } from "@/types/types";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { FaEthereum } from "react-icons/fa";
+import CardDetails from "@/components/cards/CardDetails";
 
 const CollectionPage = () => {
     const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"; 
-    const userAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
+    const userAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
     const provider = new ethers.JsonRpcProvider();
-    const  pinataGateway = process.env.NEXT_PUBLIC_GATEWAY_URL as string;
+    const pinataGateway = process.env.NEXT_PUBLIC_GATEWAY_URL as string;
 
-    const [selectedCard, setSelectedCard] = useState<PokemonMetadata | null>(null);
+    const [selectedCard, setSelectedCard] = useState<{ card: PokemonMetadata, tokenID: number } | null>(null);
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const { cards, loading } = useFetchOwnedCards(
@@ -24,57 +28,73 @@ const CollectionPage = () => {
     );
 
     if (loading) {
-        return <div>Loading...</div>;
+        return <p className="flex bg-zinc-900 min-h-[92vh] w-full justify-center items-center my-auto font-semibold text-2xl text-zinc-200">Loading your collection...</p>;
     }
 
-
-    const openModal = (card: PokemonMetadata) => {
-        setSelectedCard(card);
+    const openModal = (card: PokemonMetadata, tokenID: number) => {
+        setSelectedCard({ card, tokenID });
         setIsDialogOpen(true);
+    };
+
+    const handleBuy = (tokenID: number) => {
+        console.log(`Buying card with tokenID: ${tokenID}`);
     };
 
     return (
         <div className="flex bg-zinc-900 items-center justify-center min-h-[92vh] p-10">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-                {cards.map((card, index) => (
-                    <div
-                        key={index}
-                        onClick={() => openModal(card)}
-                        className="cursor-pointer transition-transform transform hover:scale-110 duration-300"
-                    >
-                        <div className="w-[250px] h-[350px] relative">
-                            <Image
-                                src={card.imageURL}
-                                alt={card.name}
-                                layout="fill"
-                                objectFit="cover"
-                                className="rounded-md"
-                            />
+                {Object.keys(cards).map((tokenID) => {
+                    const card = cards[tokenID];
+                    return (
+                        <div
+                            key={tokenID}
+                            onClick={() => openModal(card, tokenID)}
+                            className="cursor-pointer transition-transform transform hover:scale-110 duration-300"
+                        >
+                            <div className="w-[250px] h-[350px] relative">
+                                <Image
+                                    src={card.imageURL}
+                                    alt={card.name}
+                                    fill
+                                    objectFit="cover"
+                                    className="rounded-md"
+                                />
+                            </div>
                         </div>
-                        <h3 className="mt-6 text-xl text-white font-semibold text-center">
-                            {card.name}
-                        </h3>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
+
             {selectedCard && (
                 <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-                <DialogContent className='flex flex-col bg-transparent border-none w-full'>
-                    <DialogTitle className="text-2xl">{selectedCard.name}</DialogTitle>
-                    <div className="w-[350px] h-[490px] relative mb-4 mx-auto">
-                        <Image
-                        src={selectedCard.imageURL}
-                        alt={selectedCard.name}
-                        width={350} 
-                        height={490}
-                        objectFit="cover"
-                        className="rounded-md"
-                        />
-                    </div>
-                </DialogContent>
-                <DialogClose asChild>
-                    <button className="absolute top-2 right-2 text-xl">&times;</button>
-                </DialogClose>
+                    <DialogContent className='flex flex-col bg-zinc-900 border-none min-w-[50%] min-h-[50%] [&>button]:hidden'>
+                        <DialogTitle className="text-2xl text-center text-zinc-200 font-mono">
+                            {selectedCard.card.name}
+                        </DialogTitle>
+                        <div className="flex flex-row space-x-8 justify-center">
+                            <div className="w-[350px] h-[490px] relative mb-4">
+                                <Image
+                                    src={selectedCard.card.imageURL}
+                                    alt={selectedCard.card.name}
+                                    width={350} 
+                                    height={490}
+                                    objectFit="cover"
+                                    className="rounded-md"
+                                />
+                            </div>
+                            <Separator orientation="vertical" className="flex bg-zinc-700 h-[50vh] my-auto"/>
+                            <CardDetails card={selectedCard.card}/>
+                        </div>
+                        <div>
+                            <Button 
+                                className="flex mx-auto bg-teal-600 hover:bg-teal-700 font-mono"
+                                onClick={() => handleBuy(selectedCard.tokenID)}
+                            >
+                                <FaEthereum/>
+                                0.2 Buy 
+                            </Button>
+                        </div>
+                    </DialogContent>
                 </Dialog>
             )}
         </div>
