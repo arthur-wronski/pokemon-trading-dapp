f72f3b8b26d80c2250baeadada255f755b33db2f
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-02-19 16:58:16 +0000

    [FEAT] change for new state

--- client/src/components/cards/CardDetails.tsx
+++ client/src/components/cards/CardDetails.tsx
@@ -1,4 +1,4 @@
-import { PokemonMetadata } from "@/types/types"
+import { PokemonCard } from "@/types/types"
 import {
     Card,
     CardContent,
@@ -9,19 +9,19 @@ import { Label } from "@/components/ui/label"
 import { Copy } from "lucide-react"
 import { formatAddress } from "@/utils/utils"
 import { pokemonTypes, rarityToColourMap } from "@/data/data"
+import { Separator } from "@/components/ui/separator"
 
 interface CardDetailsProps {
-    card: PokemonMetadata
+    card: PokemonCard
 }
 
 const CardDetails = ({card}: CardDetailsProps) => {
-    const userAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-
     return(
         <Card className="bg-zinc-950 w-[275px] h-[490px] text-zinc-200 border-zinc-700">
-            <CardHeader>
+            <CardHeader className="pb-0">
                 <CardTitle className="text-center">Card Details</CardTitle>
             </CardHeader>
+            <Separator className="bg-zinc-700 my-4"/>
             <CardContent>
                 <form>
                 <div className="flex flex-col space-y-5 w-full font-semibold">
@@ -29,17 +29,17 @@ const CardDetails = ({card}: CardDetailsProps) => {
                         <Label className="font-mono text-zinc-400">Owner</Label>
                         <div className="flex flex-row space-x-3 text-zinc-300 items-center">
                             <Copy size={16}/>
-                            <p className="text-center font-mono"> {formatAddress(userAddress)} </p>
+                            <p className="text-center font-mono"> {formatAddress(card.owner)} </p>
                         </div> 
                     </div>
                     <div className="flex flex-col space-y-1.5">
                         <Label className="font-mono text-zinc-400">Health Points</Label>
-                        <p className="text-center text-2xl"> {card.hp} </p>
+                        <p className="text-center text-2xl"> {card.metadata.hp.toLocaleString()} </p>
                     </div>
                     <div className="flex flex-col space-y-1.5">
                         <Label className="font-mono text-zinc-400">Types</Label>
                         <p className="text-center">
-                            {card.types
+                            {card.metadata.types
                                 .map((type) => {
                                     const pokemonType = pokemonTypes.find(t => t.type === type);
 
@@ -53,24 +53,24 @@ const CardDetails = ({card}: CardDetailsProps) => {
                                         </span>
                                     );
                                 })
-                                .reduce((prev, curr) => [prev, ', ', curr])}
+                                .reduce((prev, curr) => [prev, ' + ', curr])}
                         </p>
                     </div>
                     <div className="flex flex-col space-y-1.5">
                         <Label className="font-mono text-zinc-400">Attack Name</Label>
-                        <p className="text-center"> {card.attackName} </p>
+                        <p className="text-center"> {card.metadata.attackName} </p>
                     </div>
                     <div className="flex flex-col space-y-1.5">
                         <Label className="font-mono text-zinc-400">Attack Damage</Label>
-                        <p className="text-center text-xl"> {card.attackDamage} </p>
+                        <p className="text-center text-xl"> {card.metadata.attackDamage} </p>
                     </div>
                     <div className="flex flex-col space-y-1.5">
                         <Label className="font-mono text-zinc-400">Rarity</Label>
                         <p 
                             className={`text-center font-bold animate-pulse duration-[5s]`}
-                            style={{ color: rarityToColourMap[card.rarity] }}
+                            style={{ color: rarityToColourMap[card.metadata.rarity] }}
                         >
-                            {card.rarity}
+                            {card.metadata.rarity}
                         </p>
                     </div>
                 </div>
