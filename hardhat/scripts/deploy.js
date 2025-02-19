639cdc7edbfe74ba81f2620f3da4df1f42af6246
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-02-19 16:55:14 +0000

    [FEAT] deploy script with marketplace

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,18 +1,28 @@
 async function main() {
-    // Get the contract to deploy
-    const PokemonCard = await ethers.getContractFactory("PokemonCard");
-    console.log("Deploying PokemonCard...");
-  
-    // Deploy the contract
-    const pokemonCard = await PokemonCard.deploy();
-  
-    console.log("PokemonCard deployed to:", pokemonCard.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
+  // Get the contract factories
+  const PokemonCard = await ethers.getContractFactory("PokemonCard");
+  console.log("Deploying PokemonCard...");
+
+  // Deploy PokemonCard
+  const pokemonCard = await PokemonCard.deploy();
+  await pokemonCard.waitForDeployment();  // Wait for deployment to be mined
+  const pokemonCardAddress = await pokemonCard.getAddress();
+  console.log("PokemonCard deployed to:", pokemonCardAddress);
+
+  // Deploy Marketplace with PokemonCard address
+  const PokemonMarketplace = await ethers.getContractFactory("PokemonMarketplace");
+  console.log("Deploying PokemonMarketplace...");
+  const pokemonMarketplace = await PokemonMarketplace.deploy(pokemonCardAddress);
+  await pokemonMarketplace.waitForDeployment();  // Wait for deployment to be mined
+  const marketplaceAddress = await pokemonMarketplace.getAddress();
+  console.log("PokemonMarketplace deployed to:", marketplaceAddress);
+
+  console.log("Deployment completed!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
       console.error(error);
       process.exit(1);
-    });
-  
+  });
